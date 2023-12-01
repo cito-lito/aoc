0674c57c9f409deebec07f73c11149a9f0711800
Mario Maita <thetechpomelo@gmail.com>
2023-12-01 15:46:24 +0100

    part 1

--- day-01/target/debug/deps/day_01-2c86e6fc0ba734dd.d
+++
@@ -1,5 +0,0 @@
-/Users/mmo/fun/Rust/advent-of-code-2023/day-01/target/debug/deps/day_01-2c86e6fc0ba734dd: src/main.rs
-
-/Users/mmo/fun/Rust/advent-of-code-2023/day-01/target/debug/deps/day_01-2c86e6fc0ba734dd.d: src/main.rs
-
-src/main.rs:
--- day-01/target/debug/deps/part1-5c8192f1e14cbf2a.d
+++
@@ -1,6 +0,0 @@
-/Users/mmo/fun/Rust/advent-of-code-2023/day-01/target/debug/deps/part1-5c8192f1e14cbf2a: src/bin/part1.rs src/bin/../../input-01.txt
-
-/Users/mmo/fun/Rust/advent-of-code-2023/day-01/target/debug/deps/part1-5c8192f1e14cbf2a.d: src/bin/part1.rs src/bin/../../input-01.txt
-
-src/bin/part1.rs:
-src/bin/../../input-01.txt:
