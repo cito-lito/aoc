1fe7d35e9e031c7870e90bcb7c595e1084eafe4c
Mario Maita <thetechpomelo@gmail.com>
2023-12-04 19:55:46 +0100

    day 02

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 fn main() {
     let text = include_str!("../input-01.txt");
     resolve(text);
+    resolve2(text);
 }
 
 fn resolve(text: &str) -> u32 {
@@ -31,6 +32,19 @@ fn resolve(text: &str) -> u32 {
     println!("IDs: {}", ids);
     ids
 }
+fn resolve2(text: &str) -> u64 {
+    let mut result = 0;
+    for line in text.lines() {
+        let max_values = get_max_value(line);
+        let mut product = 1;
+        for (_, &val) in max_values.iter() {
+            product *= val as u64;
+        }
+        result += product;
+    }
+    println!("Result: {}", result);
+    result
+}
 
 fn get_game_count(line: &str) -> HashMap<String, u32> {
     let re = Regex::new(r"(\d+)\s*(green|red|blue)").unwrap();
@@ -42,6 +56,19 @@ fn get_game_count(line: &str) -> HashMap<String, u32> {
         *counts.entry(color).or_insert(0) += count;
     }
     counts
+}
+
+fn get_max_value(line: &str) -> HashMap<String, u32> {
+    let re = Regex::new(r"(\d+)\s*(green|red|blue)").unwrap();
+    let mut max_values = HashMap::new();
+
+    for cap in re.captures_iter(line) {
+        let color = cap[2].to_string();
+        let count: u32 = cap[1].parse().expect("Not a number");
+        let entry = max_values.entry(color).or_insert(0);
+        *entry = count.max(*entry);
+    }
+    max_values
 }
 
 #[cfg(test)]
